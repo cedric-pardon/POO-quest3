d7464b908e070f89149beca3cb45743bcf1b60f3
Cedric Pardon <cedricp02@gmail.com>
2021-10-22 22:19:43 +0200

    poo3

---
+++ Car.php
@@ -0,0 +1,55 @@
+<?php
+
+class Car extends Vehicle
+{
+
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
+
+    private bool $isEngineOn = false;
+
+    private string $energy;
+
+    private int $energyLevel;
+
+    public function __construct(string $color, int $nbSeats, string $energy)
+{
+    parent::__construct($color, $nbSeats);
+    $this->energy = $energy;
+}
+
+    public function start()
+    {
+        if($this->isEngineOn) {
+            return 'Motor is on';
+        } else {
+            $this->isEngineOn = true;
+            return 'Motor started';
+        }
+    }
+
+    public function getEnergy(): string
+    {
+        return $this->energy;
+    }
+
+    public function setEnergy(string $energy): Car
+    {
+        if (in_array($energy, self::ALLOWED_ENERGIES)) {
+            $this->setEnergy($energy);
+        }
+        return $this;
+    }
+
+    public function getEnergyLevel(): int
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel): void
+    {
+        $this->energyLevel = $energyLevel;
+    }
+}
